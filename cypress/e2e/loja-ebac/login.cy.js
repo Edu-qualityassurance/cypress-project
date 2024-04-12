6a34a3cc273e57233e83c4ba1518d8a8bee18e90
Eduardo Teixeira <edu.qualityassurance@gmail.com>
2024-04-12 09:07:54 -0300

    faker e variáveis

--- cypress/e2e/loja-ebac/login.cy.js
+++ cypress/e2e/loja-ebac/login.cy.js
@@ -14,7 +14,8 @@ describe('Funcionalidade: Login', () => {
         cy.get('#username').type('edu.engineersoftware@gmail.com')
         cy.get('#password').type('teste123')
         cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2) > :nth-child(1)').should('contain', 'edu.engineersoftware')
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'edu.engineersoftware')
     
     })
 
