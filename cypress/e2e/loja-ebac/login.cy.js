5a7ab58387973b03d17d4e7c82c288dbfa0c88ab
Eduardo Teixeira <edu.qualityassurance@gmail.com>
2024-04-12 11:31:45 -0300

    massadedados

--- cypress/e2e/loja-ebac/login.cy.js
+++ cypress/e2e/loja-ebac/login.cy.js
@@ -1,8 +1,9 @@
 /// <reference types="cypress"/>
+const perfil = require('../../fixtures/perfil.json')
 
 describe('Funcionalidade: Login', () => {
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
         
     });
 
@@ -14,7 +15,6 @@ describe('Funcionalidade: Login', () => {
         cy.get('#username').type('edu.engineersoftware@gmail.com')
         cy.get('#password').type('teste123')
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'edu.engineersoftware')
     
     })
@@ -36,6 +36,25 @@ describe('Funcionalidade: Login', () => {
             //cy.get('.woocommerce-error > li').should('exist')
 
         
+    });
+
+    it('Deve fazer login com sucesso - Usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'edu.engineersoftware')
+    });
+
+    it.only('Deve fazer login com sucesso - Usando fixture', () => {
+        cy.fixture('perfil').then( dados => {
+            cy.get('#username').type(dados.usuario , { log: false})
+            cy.get('#password').type(dados.senha , { log: false})
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'edu.engineersoftware')
+            
+
+
+        })
     });
 
 })
