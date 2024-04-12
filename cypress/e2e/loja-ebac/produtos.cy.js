b5ef0fe0114503b682b3685ddd1ceff8d7eafded
Eduardo Teixeira <edu.qualityassurance@gmail.com>
2024-04-12 09:54:16 -0300

    validando produtos

---
+++ cypress/e2e/loja-ebac/produtos.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+    before(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.products > .row')
+            //.first()
+            //.last()
+            //.eq(4)
+            .contains('Arcadio Gym Short')
+            .click()
+            cy.get('#tab-title-description > a').should('contain', 'Descrição')
+
+        
+    });
+});
