5239bb89f76eebdb4ee8abbecb88861860a8a975
Eduardo Teixeira <edu.qualityassurance@gmail.com>
2024-04-17 18:33:46 -0300

    page objects

--- cypress/e2e/loja-ebac/produtos.cy.js
+++ cypress/e2e/loja-ebac/produtos.cy.js
@@ -1,20 +1,29 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
 
-    before(() => {
-        cy.visit('produtos')
+    beforeEach(() => {
+        produtosPage.visitarUrl()
     });
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.products > .row')
-            //.first()
-            //.last()
-            //.eq(4)
-            .contains('Arcadio Gym Short')
-            .click()
-            cy.get('#tab-title-description > a').should('contain', 'Descrição')
+        produtosPage.buscarProdutoLista('Abominable Hoodie')            
+            cy.get('#tab-title-description > a').should('contain', 'Descrição')        
+    });
 
+    it('Deve buscar um produto com sucesso', () => {
+        let produto = 'Zeppelin Yoga Pant'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto )
+    });
+
+    it('Deve visitar a página do produto', () => {
         
     });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        
+    });
+
 });
